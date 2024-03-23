1dea656d559d695f2168c7c51b5fe78d515a3f0e
Ahmedrali1 <ahmedrali97@gmail.com>
2024-03-24 02:36:10 +0300

    navbar is now a sidebar that collapses

--- talentSphere/src/app/components/navbar/navbar.component.ts
+++ talentSphere/src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  // No toggle logic needed
+  isSidebarOpen = false;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+    document.body.classList.toggle('sidebar-open', this.isSidebarOpen);
+  }
 }
