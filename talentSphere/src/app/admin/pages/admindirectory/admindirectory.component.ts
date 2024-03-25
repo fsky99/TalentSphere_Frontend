8ab34f3b0ea4a0d94063dfba76a81806e141900c
fatima <fatimafouad9999@gmail.com>
2024-03-26 02:08:10 +0300

    admin can view the directory of employees

--- talentSphere/src/app/admin/pages/admindirectory/admindirectory.component.ts
+++ talentSphere/src/app/admin/pages/admindirectory/admindirectory.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TalentService } from '../../../talent.service';
 
 @Component({
   selector: 'app-admindirectory',
   templateUrl: './admindirectory.component.html',
   styleUrl: './admindirectory.component.css'
 })
-export class AdmindirectoryComponent {
+export class AdmindirectoryComponent implements OnInit {
+constructor(private talent : TalentService){}
+ngOnInit() {
+  this.getUsers()
+}
+results: any =[]
+// getUsers(){
+//   this.talent.getAllUsers().subscribe((res:any)=>{
+// this.results = res.data
+//   })
+//   console.log(this.results)
+// }
+
+
+userData: any = [];
+AllSalaries: any = [];
+jobInfo:any = []
+empleave:any=[]
+bonuses: any =[]
+combinedData: any = [];
+
+async getUsers() {
+
+  this.talent.getAllUsers().subscribe((res:any) => {
+    this.userData = res.data;
+    this.combineData();
+  });
+
+  this.talent.getEmployeesHobInfo().subscribe((results:any)=>{
+    this.jobInfo = results.data
+    this.combineData()
+  })
+
+}
+
+combineData() {
+  
+  if (this.userData.length > 0 && this.jobInfo.length > 0) {
+
+    this.combinedData = [];
+    this.userData.forEach((user: any) => {
+     
+      const jobInformation = this.jobInfo.find((job:any)=>job.userID === user.id)
+      
+      
+   
+     
+        this.combinedData.push({ ...user,jobInformation });
+      
+    });
+
+    console.log(this.combinedData);
+  }
+}
+
+
+
+
+
 
 }
